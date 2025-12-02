25ff60d80a7df49b07011d14dd5d0e049dcd398c
jaxontn <jaxontn.tech@gmail.com>
2025-12-03 00:05:53 +0800

    moved and configure the test folder

---
+++ apps/nest-workspace/src/test/app.spec.ts
@@ -0,0 +1,37 @@
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../app/app.module";
+import request from 'supertest';
+
+describe('General API', () => {
+  let app: INestApplication;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let server: any;
+
+  async function setup() {
+    const module = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    app.setGlobalPrefix('api');
+
+    await app.init();
+    server = app.getHttpServer();
+  }
+
+  beforeEach(async () => {
+    await setup();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+  
+  describe('GET /', () => {
+    it('should return 200 OK', async () => {
+      const response = await request(server).get('/api');
+      expect(response.status).toBe(200);
+    });
+  });
+});
